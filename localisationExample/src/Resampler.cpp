0d8bad463b07776ce6a5e73f7ba60c7d95e2ec25
KdVos <50488480+KdVos@users.noreply.github.com>
2022-03-14 14:30:57 +0100

    Add Second Resampling Method and a Readme file for Parameter Options

--- localisationExample/src/Resampler.cpp
+++ localisationExample/src/Resampler.cpp
@@ -28,20 +28,22 @@ void Resampler::_multinomial(ParticleList &Particles, int N)
     ParticleList OldParticles = Particles;    
     Particles.clear();
 
+    // Compute Cumulative Sum Vector of likelihoods
+    std::vector<double long> Q;
+    Q.reserve(OldParticles.size());
+    long double runningVar = 0;
+
+    for(int i = 0; i<OldParticles.size(); i++)
+    {
+        runningVar += OldParticles[i].getWeight();
+        Q.push_back(runningVar);
+    }
+
     while (n < N)
     {
         // Draw random sample
         std::uniform_real_distribution<double> distribution(1e-6,1);
         double u = distribution(*_generatorPtr);
-        // Compute Cumulative Sum Vector of likelihoods
-        std::vector<double long> Q;
-        Q.reserve(OldParticles.size());
-        long double runningVar = 0;
-        for(int i = 0; i<OldParticles.size(); i++)
-        {
-            runningVar += OldParticles[i].getWeight();
-            Q.push_back(runningVar);
-        }
         // Find corresponding Particle  
         int m = 0;
         while ( Q[m]<u ) 
@@ -60,9 +62,42 @@ void Resampler::_multinomial(ParticleList &Particles, int N)
 
 void Resampler::_stratified(ParticleList &Particles, int N)
 {
+    // Counters
+    int n = 0;
+    int m = 0;
 
+    // Clear current Particles, save old particles
+    ParticleList OldParticles = Particles;    
+    Particles.clear();
 
+    // Compute Cumulative Sum Vector of likelihoods
+    std::vector<double long> Q;
+    Q.reserve(OldParticles.size());
+    long double runningVar = 0;
 
+    for(int i = 0; i<OldParticles.size(); i++)
+    {
+        runningVar += OldParticles[i].getWeight();
+        Q.push_back(runningVar);
+    }
 
+    while (n < N)
+    {
+        // Draw random sample
+        std::uniform_real_distribution<double> distribution(1e-10,1/N);
+        double u0 = distribution(*_generatorPtr);
+        double u  = u0 + double(n)/double(N);
 
+        while(Q[m] < u) 
+        {
+            m++;
+        }
+
+        Particle particleM = OldParticles[m];
+        particleM.setWeight(1.0/(double(N)));
+        Particles.push_back(particleM);
+
+        n++;
+    }
+    return;
 }
