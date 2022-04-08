9c9b252d7dcc4fb51db2543f90edcdb2a39caa5f
Peter van Dooren <p.v.dooren@tue.nl>
2022-04-08 14:49:40 +0200

    add io example

---
+++ io_example/src/odom.cpp
@@ -0,0 +1,64 @@
+/**
+ * Example code for the MRC (formerly EMC) course.
+ * Pico (the robot) starts driving forward using its drive forward skill.
+ * In the meantime, it continuously monitors its distance to the wall using its detection skill. 
+ * When a wall is detected, Pico can not drive forward anymore and needs to turn. 
+ * The state switches and another set of skills is called: drive backward and rotate.
+ */
+
+#include <emc/io.h>
+#include <emc/rate.h>
+#include <iostream>
+#include <math.h>
+
+#define EXECUTION_RATE 3
+
+int main(int argc, char *argv[])
+{
+    // Initialize the robot
+    emc::Rate r(EXECUTION_RATE);
+    emc::IO io;
+
+
+    emc::OdometryData odomData;
+    emc::OdometryData odomUpdate;
+
+    double distanceDriven = 0.0;
+
+    std::cout << "Displaying odometery measurements" << std::endl;
+
+    while(io.ok())
+    {
+        if(io.readOdometryData(odomData)) {
+            break;
+        }
+        r.sleep();
+    }
+
+    /* Main Execution Loop, 
+     * this loop keeps running until io.ok returns false, i.e. when a robot error occurs. */
+    while(io.ok())
+    {
+        if(io.readOdometryData(odomUpdate)) {
+            std::cout  << "odom x: " << odomUpdate.x
+                       << " y: " << odomUpdate.y
+                       << " a: " << odomUpdate.a << std::endl;
+
+            double distForward = cos(odomData.a)*(odomUpdate.x - odomData.x) + sin(odomData.a)*(odomUpdate.y - odomData.y) ;
+            double delta_time = odomUpdate.timestamp - odomData.timestamp;
+
+            distanceDriven += distForward;
+            double velocity = distForward/delta_time;
+
+            std::cout  << "distance driven: " << distanceDriven
+                       << " forward velocity: " << velocity << std::endl;
+
+            odomData = odomUpdate;
+        }
+
+        // Use this to ensure an execution rate of 20 Hertz
+        r.sleep();
+    }
+
+    return 0;
+}
