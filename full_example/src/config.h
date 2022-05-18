2ac025243cf1fb7467fe5d70e1e29e20fb478db2
Peter van Dooren <p.v.dooren@tue.nl>
2022-05-18 13:32:29 +0200

    reduce robot speed

--- full_example/src/config.h
+++ full_example/src/config.h
@@ -3,14 +3,14 @@
 #define MAX_RANGE_LRF     30.0       // [m]
 
 // forward driving
-#define FORWARD_SPEED     0.5        // [m/s]
+#define FORWARD_SPEED     0.2        // [m/s]
 #define MIN_DIST_TO_WALL  0.4        // [m]
 
 // backward driving
-#define BACKWARD_SPEED    0.2        // [m/s]
+#define BACKWARD_SPEED    0.1        // [m/s]
 #define DIST_BACKWARDS    0.1        // [m]
 #define BUMPER_TIMEOUT    0.2        // [s] maximum time the bumper signal may be lost
 
 // rotating
-#define ROTATE_SPEED      -1.0       // [rad/s]
+#define ROTATE_SPEED      -0.4       // [rad/s]
 #define ROTATED_ANGLE     0.5 * M_PI // [rad]
