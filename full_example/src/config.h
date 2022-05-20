ebbbe92fd9f86eb89b200ef0060e63d3b69be25e
peter van Dooren <p.v.dooren@tue.nl>
2022-05-20 07:34:54 +0000

    change config

--- full_example/src/config.h
+++ full_example/src/config.h
@@ -1,16 +1,15 @@
-#define EXECUTION_RATE    20         // [Hz]
+#define EXECUTION_RATE    10         // [Hz]
 // hardware config
 #define MAX_RANGE_LRF     30.0       // [m]
 
 // forward driving
-#define FORWARD_SPEED     0.2        // [m/s]
+#define FORWARD_SPEED     0.5        // [m/s]
 #define MIN_DIST_TO_WALL  0.4        // [m]
 
 // backward driving
-#define BACKWARD_SPEED    0.1        // [m/s]
-#define DIST_BACKWARDS    0.1        // [m]
-#define BUMPER_TIMEOUT    0.2        // [s] maximum time the bumper signal may be lost
+#define BACKWARD_SPEED    0.3        // [m/s]
+#define DIST_BACKWARDS    1.0        // [m]
 
 // rotating
-#define ROTATE_SPEED      -0.4       // [rad/s]
+#define ROTATE_SPEED      -0.5       // [rad/s]
 #define ROTATED_ANGLE     0.5 * M_PI // [rad]
